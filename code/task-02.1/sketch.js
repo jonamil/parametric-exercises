3aa5fd6bf92ecc461d9ac116aa33f7d115dd8fae
Jona <8698248+jonamil@users.noreply.github.com>
2021-11-06 19:47:49 +0100

    Update task 02.1: Rename variables

--- code/task-02.1/sketch.js
+++ code/task-02.1/sketch.js
@@ -3,11 +3,11 @@ const canvasWidth = 400,
       circleRadius = 150,
       decelerationFactor = 20000;
 
-let lineCount,
-    lineWeight,
+let noodleCount,
+    noodleWeight,
     vertexCount,
     backgroundColor,
-    lineColors;
+    noodleColors;
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
@@ -21,16 +21,16 @@ function setup() {
 
 function draw() {
   background(backgroundColor[0], backgroundColor[1], backgroundColor[2]);
-  strokeWeight(lineWeight);
+  strokeWeight(noodleWeight);
 
-  for (let l = 0; l < lineCount; l++) {
-    stroke(lineColors[l][0], lineColors[l][1], lineColors[l][2]);
+  for (let n = 0; n < noodleCount; n++) {
+    stroke(noodleColors[n][0], noodleColors[n][1], noodleColors[n][2]);
 
     beginShape();
 
     for (let v = 0; v < vertexCount; v++) {
       let angle = noise(
-        l / lineCount * 256 + millis() / decelerationFactor,
+        n / noodleCount * 256 + millis() / decelerationFactor,
         v / (vertexCount * 2 - 2) * 256
       ) * 720;
 
@@ -39,7 +39,7 @@ function draw() {
 
       if (v > 0 && v < vertexCount - 1) {
         radius = noise(
-          l / lineCount * 256 + millis() / decelerationFactor,
+          n / noodleCount * 256 + millis() / decelerationFactor,
           (vertexCount - 1 + v) / (vertexCount * 2 - 2) * 256
         ) * circleRadius * 1.15;
         outer = false;
@@ -63,13 +63,13 @@ function addVertex(angle, radius, outer) {
 }
 
 function randomizeParameters() {
-  lineCount = 5 + pow(3, floor(random(1, 5)));
-  lineWeight = 10 + floor(random(9));
+  noodleCount = 5 + pow(3, floor(random(1, 5)));
+  noodleWeight = 10 + floor(random(9));
   vertexCount = floor(random(4, 7));
 
   backgroundColor = [random(360), 30, 30];
-  lineColors = [];
-  for (let l = 0; l < lineCount; l++) {
-    lineColors[l] = [random(360), random(30, 70), random(70, 100)];
+  noodleColors = [];
+  for (let l = 0; l < noodleCount; l++) {
+    noodleColors[l] = [random(360), random(30, 70), random(70, 100)];
   }
 }
